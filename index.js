d52db87a2d55edcaa8c0a27144798a266af51136
Thais-Abe <abe.c.thais@gmail.com>
2024-10-10 15:40:52 -0300

    add Readme

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ function classifyHeroes(name,xpQuantity){
     }else if(xpQuantity >= 9001 && xpQuantity <= 10000){
         return `Olá ${name}! Seu nível é Imortal`;
     }else if( xpQuantity >= 8001 && xpQuantity <= 9000){
-        return `Olá ${name}! Seu nível é Ascendente`;
+        return `Olá ${name}! Seu nível é de Ascendente`;
     }else if( xpQuantity >= 7001 && xpQuantity <= 8000){
-        return `Olá ${name}! Seu nível é Platina `;
+        return `Olá ${name}! Seu nível é de Platina `;
     }else if( xpQuantity >= 6000 && xpQuantity <= 7000){
-        return `Olá ${name}! Seu nível é Ouro`;
+        return `Olá ${name}! Seu nível é de Ouro`;
     }else if( xpQuantity >= 2001 && xpQuantity <=5999){
-        return `Olá ${name}! Seu nível é Prata`;
+        return `Olá ${name}! Seu nível é de Prata`;
     }else if(xpQuantity >= 1001 && xpQuantity <= 2000 ){
-        return `Olá ${name}! Seu nível é Bronze`;
+        return `Olá ${name}! Seu nível é de Bronze`;
     }else {
-        return `Olá ${name}! Seu nível é Ferro`;
+        return `Olá ${name}! Seu nível é de Ferro`;
     }
 }
 
