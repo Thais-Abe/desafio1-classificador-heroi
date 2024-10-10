78a0d96eae26f91042b5aabf0920146943b22665
Thais-Abe <abe.c.thais@gmail.com>
2024-10-10 16:20:59 -0300

    fix code

--- desafio2/index.js
+++ desafio2/index.js
@@ -1,19 +1,19 @@
 function getRank(name,victories,defeats ){
   let result = victories - defeats;
     if(result >= 101){
-        return `Olá ${name}! Seu nível é Imortal`;
+        return `O Herói tem de saldo de ${result}! está no nível de Imortal`;
     }else if(result >= 91 && result <= 100){
-        return `Olá ${name}! Seu nível é lendário`;
+        return `O Herói tem de saldo de ${result}! está no nível de lendário`;
     }else if( result >= 81 && result <= 90){
-        return `Olá ${name}! Seu nível é de Diamante`;
+       return `O Herói tem de saldo de ${result}! está no nível de  Diamante`;
     }else if( result>= 51 && result <= 80){
-        return `Olá ${name}! Seu nível é de Ouro `;
+        return `O Herói tem de saldo de ${result}! está no nível de Ouro `;
     }else if( result >= 21 && result <= 50){
-        return `Olá ${name}! Seu nível é de Prata`;
+       return `O Herói tem de saldo de ${result}! está no nível de  Prata`;
     }else if( result >= 11 && result <=20){
-        return `Olá ${name}! Seu nível é de Bronze`;
+       return `O Herói tem de saldo de ${result}! está no nível de  Bronze`;
     }else{
-        return `Olá ${name}! Seu nível é de Ferro`;
+        return `O Herói tem de saldo de ${result}! está no nível de Ferro`;
     }
 }
 
