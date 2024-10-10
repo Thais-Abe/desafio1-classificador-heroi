0bf3459738efe9ca78a434f05183fec69d4bdd8b
Thais-Abe <abe.c.thais@gmail.com>
2024-10-10 15:39:32 -0300

    solve the problem

---
+++ index.js
@@ -0,0 +1,24 @@
+let name = " ";
+let xpQuantity = 0;
+
+function classifyHeroes(name,xpQuantity){
+    console.log(`Bem-vindo(a)${name}! `)
+    if(xpQuantity >= 100001){
+        return `Olá ${name}! Seu nível é Radiante`;
+    }else if(xpQuantity >= 9001 && xpQuantity <= 10000){
+        return `Olá ${name}! Seu nível é Imortal`;
+    }else if( xpQuantity >= 8001 && xpQuantity <= 9000){
+        return `Olá ${name}! Seu nível é Ascendente`;
+    }else if( xpQuantity >= 7001 && xpQuantity <= 8000){
+        return `Olá ${name}! Seu nível é Platina `;
+    }else if( xpQuantity >= 6000 && xpQuantity <= 7000){
+        return `Olá ${name}! Seu nível é Ouro`;
+    }else if( xpQuantity >= 2001 && xpQuantity <=5999){
+        return `Olá ${name}! Seu nível é Prata`;
+    }else if(xpQuantity >= 1001 && xpQuantity <= 2000 ){
+        return `Olá ${name}! Seu nível é Bronze`;
+    }else {
+        return `Olá ${name}! Seu nível é Ferro`;
+    }
+}
+
